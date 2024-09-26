1bd13a42042db70b276e048c8e05f857e5dd3716
Shutt3rr <bryanmora8114@gmail.com>
2024-09-26 08:18:18 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+first_name = input("Please enter your first name: ")
+middle_initial = input("Please enter youor middle initial: ")
+last_name = input("Please enter your last name: ")
+print(f"Hello, {first_name} {middle_initial} {last_name}!")
+
+
+print("Please provide the following information")
+street_address = input("Please enter street address: ")
+city = input("Please enter your city: ")
+state_initial = input("Please put your state initials: ")
+zip_code = input("Please provide your zip code: ")
+phone_number = input("Now provide your phone number: ")
+email = input("Last, please provide your email: ")
+
+print("Is all this information correct?")
+print(f"Street address: {street_address}")
+print(f"City: {city}")
+print(f"State initial: {state_initial}")
+print(f"Zip code: {zip_code}")
+print(f"Phone number: {phone_number}")
+print(f"Email: {email}")
